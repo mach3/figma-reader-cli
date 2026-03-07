4d75c1bde3e1a7cb2da432189863b804e9f68922
mach3 <mach3ss@gmail.com>
2026-03-07 15:50:59 +0900

    chore: tsup の target に es2022 を明示

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  target: "es2022",
   clean: true,
   banner: {
     js: "#!/usr/bin/env node",
